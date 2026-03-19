19ccae6d1ae1fd6382a0522d25df4488a96a360f
lenny-belardo <gheletus_raoul@yahoo.com>
2026-03-19 10:58:21 +0200

    Fix: typos in Jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,13 +2,13 @@ const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").JestConfigWithTsTest} **/
+/** @type {import("jest").JestConfigWithTsJest} **/
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__test__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  moduleNameWrapper: {
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   collectCoverageFrom: [
@@ -16,15 +16,15 @@ module.exports = {
     '!src/**/*.d.ts',
     '!src/**/*.test.ts',
     '!src/**/*.spec.ts',
-    '!src/index/.ts'
+    '!src/index.ts'
   ],
   coverageThreshold: {
-    gloabl: {
+    global: {
       branches: 70,
       functions: 70,
       lines: 70,
       statements: 70
     }
   },
-  setupFilesAfterEnv: ['<rootDir>/src/__test__/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts']
 };
