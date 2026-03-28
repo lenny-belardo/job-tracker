808f3ba382079bab248bf87e4cf33780c7f8b6e8
lenny-belardo <gheletus_raoul@yahoo.com>
2026-03-28 13:48:12 +0200

    Add AppLayout wrapper component: (1) combines AppHeader and AppSidebar; (2) horizontal layout with sidebar on left; (3) vertical layout for header and content on right; (4) main content area with padding; (5) router-view renders current page component

---
+++ frontend/src/components/layout/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex min-h-screen bg-gray-50">
+        <AppSidebar />
+
+        <div class="flex-1 flex flex-col">
+            <AppHeader />
+
+            <main class="flex-1 p-6">
+                <router-view />
+            </main>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from './AppHeader.vue';
+import AppSidebar from './AppSidebar.vue';
+</script>
